de359d04e61e73aee82559ba4476c61187a25ca3
Ariel Morelli <arielmorelli2@gmail.com>
2021-01-13 15:19:02 -0300

    Remove useless comments and update variable

--- cm_plugin_hello/__init__.py
+++ cm_plugin_hello/__init__.py
@@ -1,9 +1,7 @@
 from plugin import run
 
-# routes should use same syntax from flask add_url_rule
-#  More information here https://flask.palletsprojects.com/en/1.1.x/api/#flask.Flask.add_url_rule
 routes = [
     {"rule": "/hello_world", "view_func": run},
 ]
 
-run_func = [] # functions to run wasn't defined yet, but this variable is necessary
+scripts = []
